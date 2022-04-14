c09ec40b383c49aa0d01bcdb732e18df632c5d97
canadmin <icanyard@gmail.com>
2022-04-14 15:43:45 +0300

    refs http controller

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const Path = require("path");
 
 const PORT = process.env.PORT || 4000;
 
-
 const client = redis.createClient({url: 'redis://127.0.0.1:6379'});
 client.connect();
-
-
 
 const createAssetParam = (begin,end) => {
     let params = coins.slice(begin,end).join(",");
@@ -49,7 +46,6 @@ cron.schedule('*/5 * * * *', async () => {
             result.forEach(item => {
                cronRedisClient.set(item.symbol ,JSON.stringify(item.price));
             })
-            // cronRedisClient.set("test" ,JSON.stringify(success.data));
         })
     }
     cronRedisClient.quit();
@@ -71,6 +67,10 @@ schedule.scheduleJob('0 */4 * * *', () => {
         },
     ).then(suc => {
         let data = suc.data.data;
+        cronRedisClient.set('top10',JSON.stringify(data.slice(0,10)));
+        cronRedisClient.set('top5',JSON.stringify(data.slice(0,5)))
+        cronRedisClient.set('top100',JSON.stringify(data.slice(0,100)));
+        cronRedisClient.set('all',JSON.stringify(data));
         console.log(data)
         data.forEach(item => {
             let key = item.symbol + '-cmc';
@@ -85,19 +85,48 @@ schedule.scheduleJob('0 */4 * * *', () => {
 
 
 const getCurrenciesDataFromCryptoCompare = async (req, res, next) => {
-    const {symbol} = req.params;
-    const value = await client.get(symbol)
-    res.send(JSON.parse(value));
+    try{
+        const symbol= req.query.symbol;
+        const value = await client.get(symbol)
+        res.send(JSON.parse(value));
+    }catch (e){
+        console.log(e)
+        res.send(JSON.parse(e));
+    }
+
 }
 const getCryptoCurrencyInfo = async (req, res, next) => {
-    const {symbol} = req.params;
-    const value = await client.get(symbol+'-cmc')
-    res.send(JSON.parse(value));
+    try{
+        const symbol = req.query.symbol;
+        console.log(symbol)
+        const value = await client.get(symbol+'-cmc')
+        res.send(JSON.parse(value));
+    }catch (e){
+        console.log(e)
+        res.send(JSON.parse(e));
+    }
+
+}
+
+const getCurrencies = async (req,res,params) => {
+     try{
+         const listType = req.query.listType;
+         console.log(req.query)
+         const value = await client.get(listType)
+         res.send(JSON.parse(value));
+     }catch (e){
+         console.log(e)
+         res.send(JSON.parse(e));
+
+     }
+
 }
 
 
-app.get('/currency/:symbol',getCurrenciesDataFromCryptoCompare)
-app.get('/cryptoInfo/:symbol',getCryptoCurrencyInfo)
+app.get('/currency',getCurrenciesDataFromCryptoCompare);
+app.get('/cryptoInfo',getCryptoCurrencyInfo);
+app.get('/currencies',getCurrencies);
+
 
 
 let server = app.listen(PORT, function () {
